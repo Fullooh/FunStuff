004a0dce46e2a44a600b3e90fe6c62000051ea6b
Fullooh <58962478+Fullooh@users.noreply.github.com>
2022-06-06 02:03:22 -0400

    Update HeliGame.cpp

--- HeliGame.cpp
+++ HeliGame.cpp
@@ -1,1 +1,12 @@
+#include <math.h>
+#include <iostream.h>
+#include <conio.h>
+#include <time.h>
+#include <graphics.h>
+#include <process.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <dos.h>
+
+using namespace std;
 
