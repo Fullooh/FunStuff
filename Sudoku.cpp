88e3480d21712987971f2583412e8e6ff47fedf9
Fullooh <58962478+Fullooh@users.noreply.github.com>
2022-06-06 02:03:39 -0400

    Update Sudoku.cpp

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <cstdio>
+#include <cdstring>
+#include <cstdlib>
 
+using namespace std;
+
+#define empty 0
+#define N 9
+
+bool isGridsafe (int grid [N][N], int &row, &col);
+/* assign values to all the zero (not assigned) values for Sudoku
+    solutions */
+bool SolveSudoku (int grid [N][N])
+{
+    int row, col;
+    if (!isEmptyLocation(grid, row, col))
+        return true;
+    for (int num = 1; num <= 9; num++)
+    {
+        
+
+    }
+
+
+}
